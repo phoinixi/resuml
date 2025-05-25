c9e594ea9be1b1549beaec617b863d0fafa7ecd1
Ermis Kerykeion <ermis-cy@users.noreply.github.com>
2025-05-26 00:56:42 +0200

    chore: update release configuration and version

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  preset: 'angular',
+  tagFormat: 'v${version}',
+  releaseRules: [
+    { type: 'docs', release: 'patch' },
+    { type: 'refactor', release: 'patch' },
+    { type: 'style', release: 'patch' },
+    { type: 'test', release: 'patch' },
+  ],
+};
